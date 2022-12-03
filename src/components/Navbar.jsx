a8e83e98ca5da82d2ea64ecc372e5534c3844564
TblP AI <vczypl@e.et>
2022-12-03 19:28:25 +0300

    test

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import './styles/navbar.css'
+
+const Navbar = () => {
+    return (
+      <nav className={"navbar_tab"}>
+        <h2 className={"navbar_text"}>Gorbunov Vladimir BVT1901</h2>
+      </nav>
+    )
+}
+
+export default Navbar;
